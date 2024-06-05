ffbf47e35647eb4e742993b6caba72be733df7a9
KeenanKistoo <kistook19@gmail.com>
2024-06-05 22:02:31 +0200

    Quick Fixes

--- gamer-hub/src/Components/Subhead/Subhead.jsx
+++ gamer-hub/src/Components/Subhead/Subhead.jsx
@@ -1,37 +1,13 @@
-import {React, useEffect, useRef} from 'react';
-import {motion, useInView, useAnimation} from 'framer-motion';
+import {React} from 'react';
 import '../Subhead/Subhead.css'
 function Subhead(props){
     const {heading} = props; //This prop allows me to use this component throughout the webapp
-    const ref = useRef(null); //Framer motion requires a reference to use the useInView function
-    const isInView = useInView(ref, {once: true}); //Check when ref is in view and also how many times the animation should trigger
-
-    const subHeadControls = useAnimation(); //framer-motion gives me access to a set of built-in animation controls
-
-    useEffect(() => {
-        if(isInView){
-            subHeadControls.start("visible"); //Triggers the visible variant set below. 
-        }
-    }, [subHeadControls, isInView])
   
   return (
-        <motion.section 
-            className='home-head'
-            ref={ref}
-            variants={{
-                hidden: {opacity: 0, y: 20},
-                visible: {opacity:1, y: 0}
-            }}
-            initial="hidden"
-            animate={subHeadControls}
-            transition={{duration: 0.5, delay: 0.25}}>
-                <h2 className='sub-head-home'>{heading}</h2>
-        </motion.section>
+        <section className='home-head'>
+            <h2 className='sub-head-home'>{heading}</h2>
+        </section>
   );
 };
 
 export default Subhead;
-
-/*  I learnt how to use the framer motion components from this https://www.youtube.com/watch?v=hjbxaYTMhy0 tutorial.
-    I felt that the website itself was too static and I wanted to add a bit more fluidity to it.
-    */
--- gamer-hub/src/Pages/Home/Home.jsx
+++ gamer-hub/src/Pages/Home/Home.jsx
@@ -1,19 +1,42 @@
-import {React} from 'react';
+import {React, useEffect, useRef} from 'react';
+import {motion, useInView, useAnimation} from 'framer-motion';
 import Subhead from '../../Components/Subhead/Subhead';
 import Rank from '../../Components/Rank/Rank';
 import '../Home/Home.css'
 
 function Home(){
     
-  
+  const ref = useRef(null); //Framer motion requires a reference to use the useInView function
+    const isInView = useInView(ref, {once: true}); //Check when ref is in view and also how many times the animation should trigger
+
+    const subHeadControls = useAnimation(); //framer-motion gives me access to a set of built-in animation controls
+
+    useEffect(() => {
+        if(isInView){
+            subHeadControls.start("visible"); //Triggers the visible variant set below. 
+        }
+    }, [subHeadControls, isInView])
   return (
-    <main className='home-main'>
+    <motion.main className='home-main'
+            ref={ref}
+            variants={{
+                hidden: {opacity: 0, y: 20},
+                visible: {opacity:1, y: 0}
+            }}
+            initial="hidden"
+            animate={subHeadControls}
+            transition={{duration: 0.3, delay: 0.25}}>
        <Subhead
        heading="The Current Most Played Video Games (04/06/2024)"
        />
        <Rank/>
-    </main>
+    </motion.main>
   );
 };
 
 export default Home;
+
+
+/*  I learnt how to use the framer motion components from this https://www.youtube.com/watch?v=hjbxaYTMhy0 tutorial.
+    I felt that the website itself was too static and I wanted to add a bit more fluidity to it.
+    */
