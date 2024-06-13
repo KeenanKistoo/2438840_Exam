7088cfdc22b869d2da5d65c744a0ca16ea8635aa
KeenanKistoo <kistook19@gmail.com>
2024-06-14 00:05:58 +0200

    Added All States
    Game works fine.
    
    a few bugs need fixing:
    - Display correct answer in a better way
    -Display loss of life/earned points
    -end game conditions

--- gamer-hub/src/Components/Game/Game.jsx
+++ gamer-hub/src/Components/Game/Game.jsx
@@ -51,7 +51,14 @@ function Game() {
     //Ans. Btn State
     const [btnsActive, setBtnsActive] = useState(false);
 
-    //let timerInt = setInterval(handleTimer, 1000);
+    //Game States: wait, load, ready, listen, answer, score
+    const [gameState , setGameState] = useState('wait');
+
+    //user Anwer -> check if the user answered or not
+    const [userAns, setUserAns] = useState(false);
+
+    //Detect Btn Answers
+    const [buttonDist, setButtonDist] = useState(false);
 
     //Random Selection of 5 Songs ***Add this function to the start button.
     function genSongs(){
@@ -64,11 +71,13 @@ function Game() {
         for(let i = 0; i < copyArray.length; i++){
             copyArray[i].selected = false;
             copyArray[i].selected = false;
+
         }
 
         //Assigns 7 Random Songs
         while(TrackerNum > 0){
             let ranNum = Math.floor(Math.random () * availNumbers.length)
+            console.log(ranNum);
             copyArray[ranNum].selected = true; //Sets selected song for future usage
             availNumbers.splice(ranNum, 1); //Remove Option From Array
             TrackerNum--;   //Decrements the tracker by 1 to ensure the correct number of questions are available.
@@ -86,32 +95,33 @@ function Game() {
             }
         }
 
-        //console.log(copyArray);
+        console.log(copyArray);
         setTracker(trackerUpdate); //Update Number of Questions
         setSongs(copyArray); //Update Song List 
-        //console.log(tracker);
+        //console.log(tracker);..
 }
 
     function setButtonAnswers(){
+        setButtonDist(true);
         let copyArray = [...songs] //Copy of Audio.js
-        let answer = []; //Empty Array to store the answer
+        let selectedAnswer = []; //Empty Array to store the answer
         let answerBtns = [setAnswer1, setAnswer2, setAnswer3, setAnswer4] //Create an array for button states
         //console.log("Working")
         let copyRanSongs = [...randomSongs] //Copy of random songs
 
         for(let i = 0; i < copyArray.length; i++){
             if(copyArray[i].selected && !copyArray[i].used){
-                answer = copyArray[i];
+                selectedAnswer = copyArray[i];
                 copyArray[i].used = true; //Set used to true to avoid reusing the same answer
-                console.log("Answer Caught")
-                setAnswer(answer); //Set the answer for correct/incorrect check
+                console.log(selectedAnswer)
+                setAnswer(selectedAnswer); //Set the answer for correct/incorrect check
                 break;
             }
         }
         
         let ranIndex = Math.floor(Math.random() * 4) //Select a random btn to store answer
 
-        answerBtns[ranIndex](answer.answer); //Display answer
+        answerBtns[ranIndex](selectedAnswer.answer); //Display answer
         answerBtns.splice(ranIndex, 1); //Remove answerBtn to avoid reiterating over the same btn
         //console.log(answerBtns)
 
@@ -128,31 +138,95 @@ function Game() {
 
     function playSong(){
         let aud = new Audio(answer.audio)
-        aud.play()
+        aud.play();
+    }
+
+    function startLoad(){
+        setGameState('load')
+        console.log(gameState);
+    }
+
+    function assignScore(selectedAnswer){
+        setBtnsActive(false);
+        setGameState('score');
+        setUserAns(true);
+        if(answer.answer === selectedAnswer){
+            SetScore(prev => prev + 300)
+        }else if(answer.answer!== selectedAnswer){
+            setLives(prev => prev - 1)
+        }
     }
     //https://www.youtube.com/watch?v=4_9yJXO4F2Y
     const [countdown, setCountdown] = useState(9)
+    const [startInterval, setStartInterval] = useState(false);
     const timerId = useRef();
-    const [stateCheck , setStateCheck] = useState(false);
+
+    const [comm, setComm] = useState('');
+    const [nextState, setNextState] = useState('')
+    
+    function timerHandler(){
+        timerId.current = setInterval(() => {
+            setCountdown(prev => {
+                if (prev > 0){
+                    setNewWidth(prevWidth => prevWidth - 11)
+                    return prev - 1
+                }else if(prev <= 0) {
+                    setGameState(nextState)
+                }
+            })
+            
+        }, 1000)
+    }
     //***NOTE:comment out this hook
     useEffect(() => {
-        timerId.current = setInterval(() => {
-            if(countdown > 0){
-            setCountdown(prev => prev - 1)
-            if(stateCheck){
-            setNewWidth(prev => prev - 11)
-            }
-            }
-        }, 1000)
-        return () => clearInterval(timerId.current)
-     },[]); // This line is for testing purposes. The function needs to be called on button click. NOTE:comment out this hook
+        if(startInterval){
+            timerHandler();
+        }   
+    return () => clearInterval(timerId.current)
+    
+     },[startInterval, nextState]); // This line is for testing purposes. The function needs to be called on button click. NOTE:comment out this hook
 
      useEffect(() =>{
-        if(countdown <= 0){
-            clearInterval(timerId.current);
-            alert("END")
+        if(gameState === 'wait'){
+            setCountdown(5);
+            setNewWidth(100);
+            //console.log("Waiting For Player")
+        }else if(gameState === 'load'){
+            setComm('Loading...')
+            genSongs();
+            setCountdown(3);
+            setStartInterval(true);
+            SetScore(0);
+            setLives(3);
+            setNextState('ready');
+        }else if(gameState === 'ready'){
+            setComm('Ready?')
+            setCountdown(3);
+            if(!buttonDist){
+                setButtonAnswers();
+            }
+            setNewWidth(100);
+            setNextState('listen');
+            setUserAns(false)
+        }else if(gameState === 'listen'){
+            setComm('...')
+            setCountdown(9);
+            setNewWidth(100);
+            setNextState('answer')
+            playSong();
+        }else if(gameState === 'answer'){
+            setCountdown(5)
+            setNextState('score')
+        }else if(gameState === 'score'){
+            setButtonDist(false)
+            setCountdown(5)
+            console.log('score')
+            if(!userAns){
+                assignScore('no answer');
+            }
+            setNextState('ready')
         }
-     }, [countdown])
+     }, [gameState,userAns])
     
     return (
         <section className='arcade-screen'>
@@ -166,7 +240,7 @@ function Game() {
                 <p className="score">{score}</p>
             </section>
             <section className="icon-sect">
-                <p className="warn">Test</p>
+                <p className="warn">{comm}</p>
             </section>
             <section className="timer-sect">
                 <p className="time-txt" ref={timerId}>00:0{countdown}</p>
@@ -174,16 +248,22 @@ function Game() {
             <section className="prog-sect">
                 <div className="prog-bar" style={{width: `${newWidth}%`, transition: '0.5s ease-out linear'}}>.</div>
             </section>
-            <section className="answer-sect">
-                <button ref={button1} className="ans-btn" disabled={btnsActive ? false : true}>{answer1}</button>
-                <button ref={button2} className="ans-btn" disabled={btnsActive ? false : true}>{answer2}</button>
-                <button ref={button3} className="ans-btn" disabled={btnsActive ? false : true}>{answer3}</button>
-                <button ref={button4} className="ans-btn" disabled={btnsActive ? false : true}>{answer4}</button>
+            <section className={`answer-sect ${gameState === 'answer' || gameState === 'score' ? '' : 'hidden'}`}>
+                <button ref={button1} onClick={() => assignScore(answer1)} className= {`ans-btn ${gameState === 'score'&& answer.answer === answer1 ? 'correct': 'incorrect'}`} disabled={btnsActive ? false : true}>{answer1}</button>
+                <button ref={button2} onClick={() => assignScore(answer2)} className= {`ans-btn ${gameState === 'score' && answer.answer === answer2 ? 'correct': 'incorrect'}`} disabled={btnsActive ? false : true}>{answer2}</button>
+                <button ref={button3} onClick={() => assignScore(answer3)} className= {`ans-btn ${gameState === 'score' && answer.answer === answer2 ? 'correct': 'incorrect'}`} disabled={btnsActive ? false : true}>{answer3}</button>
+                <button ref={button4} onClick={() => assignScore(answer4)} className= {`ans-btn ${gameState === 'score' && answer.answer === answer2 ? 'correct': 'incorrect'}`} disabled={btnsActive ? false : true}>{answer4}</button>
             </section>
             <section>
-                <button onClick={() => genSongs()}>Test</button>
+                <button onClick={() => startLoad()}>Test</button>
                 <button onClick={() => setButtonAnswers()}>Test</button>
                 <button onClick={() => playSong()}>Test</button>
+            </section>
+            <section className="score-info-sect">
+                <p className="score">+300</p>
+            </section>
+            <section className="life-info-sect">
+                <p className="score">-1</p>
             </section>
         </section>
     );
